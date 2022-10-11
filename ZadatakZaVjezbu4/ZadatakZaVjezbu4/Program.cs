c871c1d4224f82ad0289e5ae9b71cc3ff6ed337b
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-10-11 20:47:35 +0200

    Add files via upload

---
+++ ZadatakZaVjezbu4/ZadatakZaVjezbu4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaVjezbu4
+{
+    internal class Program
+    {
+
+        class Sort
+        {
+            public static void QuickSort(int[] array, int left, int right)
+            {
+                int temp;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = 0; j < array.Length - 1 - i; j++)
+                    {
+                        if (array[j] < array[j + 1])
+                        {
+                            temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+                array[0] = left;
+                array[array.Length - 1] = right;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int left, right;
+            int[] array = { 2, 4, 3, 7, 6, 6, 8, 1, 9 };
+            left = array[0];
+            right = array[array.Length - 1];
+            Sort.QuickSort(array, left, right);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
